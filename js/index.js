b905eb0a903ced04b668531132662abc4a34b15f
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-16 20:08:43 +0300

    js: count the clicks

--- js/index.js
+++ js/index.js
@@ -10,36 +10,45 @@ const start = () => {
     boxes.forEach((box, i) => {
         box.addEventListener("click", () => {
             number_of_clicks += 1;
+            
             const image = document.getElementById("img"+i);
             image.classList.add("show");
 
             if (image.classList.contains("html")==true) {
-                html += 1
-            }
-            console.log(html)
+                html += 1; image.classList.add("displayed");
+                if (css != 0 || js != 0 ) { css=0, html = 0, js= 0; }
+            } 
+
             if (html == 2 ) {
                 hide_contants(0, 4);
+                number_of_clicks = 0;
+            } else { hideAllPictures(boxes, number_of_clicks); }
+            
+            if (image.classList.contains("css")==true) {
+                css += 1; ; image.classList.add("displayed");
+                if (html != 0 || js != 0 ) { css=0, html = 0, js= 0; }
             }
 
-            if (image.classList.contains("css")==true) {
-                css += 1
-            }
-            console.log(html)
             if (css == 2 ) {
                 hide_contants(1, 2);
-            }
+                number_of_clicks = 0;
+            } else { hideAllPictures(boxes, number_of_clicks);}
 
             if (image.classList.contains("js")==true) {
-                js += 1
+                js += 1; ; image.classList.add("displayed");
+                if (html != 0 || css != 0 ) { css=0, html = 0, js= 0; }
             }
-            console.log(html)
+
             if (js == 2 ) {
                 hide_contants(5, 3);
-            }
-
-
-            
-    })
+                number_of_clicks = 0;
+            } else { hideAllPictures(boxes, number_of_clicks);}
+    
+    if (number_of_pictures == 0) {
+        const score = document.getElementById("score")
+        score.innerText += "You won! Your score is 3"
+    }
+        })
 })}
 
 
@@ -48,11 +57,11 @@ const hideAllPictures = (boxes, number_of_clicks) => {
         boxes.forEach((box,i) => {
             const image = document.getElementById("img"+i);
             image.classList.remove("show")
-            box.classList.add("transparent")
     })
-}
+    }
 }
 
+// hides the pairs after founding them
 const hide_contants = (a,b) => {
     let img1 = document.getElementById("img"+a);
     let box1 = document.getElementById("box"+a);
@@ -64,9 +73,4 @@ const hide_contants = (a,b) => {
     box2.classList.add("transparent");
 }
 
-const found_html = (number) => {
-    if (number == 2) {
-        pass
-}}
-
-start()
+start();
